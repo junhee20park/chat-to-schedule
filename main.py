c8a51dd0f47b944fd204c4f5ab4fe78cd45b3f34
Junhee Park <junhee20park@berkeley.edu>
2023-06-18 00:31:44 -0700

    can fetch json file from openai

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from __future__ import print_function
 
+import asyncio
 import datetime
+import json
+import openai
 import os.path
+import requests
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -11,6 +15,30 @@ from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def gen_prompt(user_input):
+    day_name = "Sunday"
+    formatted_date = "2023-06-18"
+    return "Generate a JSON file that is an array of events named 'events'. Each event has the following fields: " \
+           "eventName, duration (in minutes), possibleIntervals[ { date, timePeriod: {startTime, endTime} ]. " \
+           "timePeriod indicates the full range of time that event could happen. Today is " + day_name + " " + \
+           formatted_date + ". Query: " + user_input
+
+
+def get_gpt_response(user_input):
+    """Parses the user_input into a JSON file using the GPT-3 model."""
+
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": gen_prompt(user_input)}],
+    )
+
+    # print(response["choices"][0]["message"])
+    # json_response = json.loads(response["choices"][0]["message"]["content"])
+    # return json_response
+    return json.loads(response.choices[0].message.content)
 
 
 def main():
@@ -35,28 +63,36 @@ def main():
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
-    try:
-        service = build('calendar', 'v3', credentials=creds)
+    # Get text input from user.
+    user_input = input("What would you like to schedule this week? ")
 
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              maxResults=10, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
+    # Parse text input from user through GPT-3 model.
+    json_file = get_gpt_response(user_input)
+    # print(json_file)
+    print(json.dumps(json_file, indent=4))
 
-        if not events:
-            print('No upcoming events found.')
-            return
-
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
-
-    except HttpError as error:
-        print('An error occurred: %s' % error)
+    # try:
+    #     service = build('calendar', 'v3', credentials=creds)
+    #
+    #     # Call the Calendar API
+    #     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    #     print('Getting the upcoming 10 events')
+    #     events_result = service.events().list(calendarId='primary', timeMin=now,
+    #                                           maxResults=10, singleEvents=True,
+    #                                           orderBy='startTime').execute()
+    #     events = events_result.get('items', [])
+    #
+    #     if not events:
+    #         print('No upcoming events found.')
+    #         return
+    #
+    #     # Prints the start and name of the next 10 events
+    #     for event in events:
+    #         start = event['start'].get('dateTime', event['start'].get('date'))
+    #         print(start, event['summary'])
+    #
+    # except HttpError as error:
+    #     print('An error occurred: %s' % error)
 
 
 if __name__ == '__main__':
